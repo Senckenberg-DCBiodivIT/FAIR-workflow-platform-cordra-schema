a991ea51e524045dd0b4473bce45990e2dbf3f30
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-05-27 14:50:32 +0200

    update scripts

--- schema/deleteAllObjects.py
+++ schema/deleteAllObjects.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from pathlib import Path
 from cordra_auth import get_auth_token
 import requests
--- schema/updateSchemas.py
+++ schema/updateSchemas.py
@@ -1,24 +1,41 @@
+#!/usr/bin/env python3
+import argparse
 import json
+from pathlib import Path
+from getpass import getpass
 
 import requests
 import os
-from pathlib import Path
 
-# ignore https warning
+# ignore https warnings
 import urllib3
 
 from cordra_auth import get_auth_token
 
 urllib3.disable_warnings()
 
-USER = "admin"
-PASSWORD = "password"
-BASE_URL = "https://localhost:8443"
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("url", nargs='?', default="https://localhost:8443")
+parser.add_argument("-u", "--user", required=False, default="admin")
+parser.add_argument("-p", "--password", required=False, default="")
+args = parser.parse_args()
+
+USER = args.user
+BASE_URL = args.url
+PASSWORD = args.password if args.password else getpass(prompt="admin password: ")
+
+# path to schema.json files
 SCHEMA_FOLDER = Path("schemas")
+
+# path to a library jar file
 LIBRARY = "../lib/build/libs/lib.jar"
+# Name of the type where the library should be attached to as java payload
 LIBRARY_SCHEMA = "Dataset"
 
-def get_schemas(token):
+
+def get_schemas(token=get_auth_token(BASE_URL, USER, PASSWORD)):
+    """ get a map of schemas present in cordra. maps schema name to schema id """
     headers = {"Authorization": f"Bearer {token}"}
     r = requests.get(BASE_URL + "/search?query=type:Schema", verify=False, headers=headers)
     name_to_id = {}
@@ -27,30 +44,29 @@ def get_schemas(token):
     return name_to_id
 
 
-def update_schema(type, schema, token, schema_id=None):
-    print(f"processing {type}: ", end="")
+def update_schema(type, schema, token=get_auth_token(BASE_URL, USER, PASSWORD), schema_id=None):
     headers = {"Authorization": f"Bearer {token}"}
 
     # parse body. Can't use requests json parameter because it does not work with multipart
     files = {"content": (None, json.dumps(schema), "application/json")}
 
     # attach java lib to body if type matches the library schema
-    if type == LIBRARY_SCHEMA:
-        print(f"attach library to schema {type}")
-        files["java"] = ("java", open(LIBRARY, "rb"), "application/octet-stream")
+    # if type == LIBRARY_SCHEMA:
+    #     print(f"attach library {LIBRARY} to schema {type}")
+    #     files["java"] = ("java", open(LIBRARY, "rb"), "application/octet-stream")
 
     # upsert schema
     if schema_id is None:  # insert
-        print(f"insert new schema")
+        print(f"Schema not found. Inserting...")
         url = BASE_URL + "/objects/?type=Schema"
         r = requests.post(url, json=schema, files=files, verify=False, headers=headers)
     else:  # update
-        print(f"update schema ({schema_id})")
+        print(f"Schema known under id {schema_id}. Updating...")
         url = BASE_URL + "/objects/" + schema_id
         r = requests.put(url, json=schema, files=files, verify=False, headers=headers)
 
     if r.status_code != 200:
-        raise Exception(f"failed to update schema for {type}: {r.json()}")
+        raise Exception(f"Failed to update Schema for {type} ({r.status_code}): {r.text}")
 
 
 token = get_auth_token(BASE_URL, USER, PASSWORD)
@@ -64,6 +80,6 @@ for filename in os.listdir(SCHEMA_FOLDER):
         schema_id = None
 
     with open(SCHEMA_FOLDER / filename, "r") as f:
-        print(filename)
+        print("Processing ", filename, "...")
         schema = json.load(f)
-    update_schema(type, schema, token, schema_id=schema_id)
+        update_schema(type, schema, token, schema_id=schema_id)
