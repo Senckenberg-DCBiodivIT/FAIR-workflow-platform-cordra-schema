e8ccae3316b4fc645ba27df94fdbac0364049e0d
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-06-07 10:33:09 +0200

    removed unused code

--- lib/src/main/kotlin/de/senckenberg/cwr/Helper.kt
+++ lib/src/main/kotlin/de/senckenberg/cwr/Helper.kt
@@ -9,32 +9,3 @@ fun applyTypeAndContext(json: JsonObject, type: String, context: String) {
     json.addProperty("@type", type)
     json.addProperty("@context", context)
 }
-
-fun deserializeIntoCordraObject(json: JsonObject, type: String): CordraObject {
-    val cordra = CordraHooksSupportProvider.get().cordraClient
-    return cordra.create(type, json)
-}
-
-fun propertyToReferences(json: JsonObject, property: String, type: String, asArray: Boolean = false) {
-    if(!json.has(property)) {
-        return
-    }
-
-    val propertyValue = json.get(property)
-    val cordraObjects = if (propertyValue.isJsonArray) {
-        propertyValue.asJsonArray.map { deserializeIntoCordraObject(it.asJsonObject, type) }
-    } else {
-        listOf(deserializeIntoCordraObject(propertyValue.asJsonObject, type))
-    }
-
-    if (asArray) {
-        val refsAsJsonArray = cordraObjects.fold(JsonArray()) { array, value ->
-            array.add(value.id)
-            array
-        }
-        json.add(property, refsAsJsonArray)
-    } else {
-        assert(cordraObjects.size == 1) { "Failed to convert $property to $type. Single object expected, but multiple found." }
-        json.addProperty(property, cordraObjects.first().id)
-    }
-}
